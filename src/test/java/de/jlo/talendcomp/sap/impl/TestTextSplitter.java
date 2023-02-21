d0ba7f64c31c8a4496b1dfc8c0989e81d502dd38
Jan Lolling <jan.lolling@gmail.com>
2023-02-21 23:04:35 +0100

    test files changed

--- src/test/java/de/jlo/talendcomp/sap/impl/TestTextSplitter.java
+++ src/test/java/de/jlo/talendcomp/sap/impl/TestTextSplitter.java
@@ -32,7 +32,7 @@ public class TestTextSplitter {
 
 	@Test
 	public void test3() {
-		String input = "abcd  efg";
+		String input = "abcd  efg;";
 		List<String> result = TextSplitter.split(input, ';');
 		for (String s : result) {
 			System.out.println(">" + s);
@@ -58,6 +58,55 @@ public class TestTextSplitter {
 			System.out.println(">" + s);
 		}
 		assertTrue("Count parts incorrect: " + result.size(), result.size() == 0);
+	}
+
+	@Test
+	public void testSplit() {
+		String s = "1100003204\b"
+				+ "000010\b"
+				+ "ZA02\b"
+				+ "20160111\b"
+				+ "000000000010135055\b"
+				+ "4110080040010190  \b"
+				+ "         1.000 \b"
+				+ "PAK\b"
+				+ "       700.000 \b"
+				+ "       700.000 \b"
+				+ "G  \b"
+				+ "          \b"
+				+ "000000\b"
+				+ "          \b"
+				+ "000000\b"
+				+ "       83.50 \b"
+				+ "       38.41 \b"
+				+ "        0.00 \b"
+				+ "        0.00 \b"
+				+ "       38.41 \b"
+				+ "         38.41 \b"
+				+ "CHF  \b"
+				+ "1710\b"
+				+ "50\b"
+				+ "000000\b"
+				+ "20210908\b"
+				+ "         39.53 \b"
+				+ "GL01  \b"
+				+ "HOBOTEC SeHoBoShr St vz 4-25TX20        \b"
+				+ "          \b"
+				+ "\b";
+//		StringTokenizer st = new StringTokenizer(s, "\b");
+//		int index = 0;
+//		while (st.hasMoreElements()) {
+//			System.out.println(index++ + ": " + st.nextElement());
+//		}
+		if (s.endsWith("\b")) { // prevent ignoring the last field is empty
+			s = s + " ";
+		}
+		String[] array = s.split("\b");
+		int index = 0;
+		for (String v : array) {
+			System.out.println(index++ + ": " + v);
+		}		
+		assertTrue("Split does not work here", index == 32);
 	}
 
 }
